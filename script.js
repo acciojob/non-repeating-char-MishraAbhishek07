1abc2de4b89f2f8f6f87337be4b61491ea347f52
MishraAbhishek07 <davmishra96@gmail.com>
2024-10-21 16:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// alert(firstNonRepeatedChar(input)); 
